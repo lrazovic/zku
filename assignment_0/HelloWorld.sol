750cfa9fd55d5bf067afb3ec78b07c12ddc1ec19
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2022-04-10 15:49:21 +0200

    Use NatSpec Format to document the HelloWorld contract

--- assignment_0/HelloWorld.sol
+++ assignment_0/HelloWorld.sol
@@ -1,26 +1,29 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Defines a contract named `HelloWorld`.
-// A contract is a collection of functions and data (its state). Once deployed, a contract resides at a specific address on the Ethereum blockchain. Learn more: https://solidity.readthedocs.io/en/v0.5.10/structure-of-a-contract.html
+/// @author Leonardo Razovic
+/// @title A simple "Hello World" contract example
 contract HelloWorld {
-
-    // Declares a state variable `number` of type `uint`.
-    // State variables are variables whose values are permanently stored in contract storage. The keyword `public` makes variables accessible from outside a contract and creates a function that other contracts or clients can call to access the value.
+    /// @dev The value stored in the contract
     uint256 number;
 
-    // Similar to many class-based object-oriented languages, a constructor is a special function that is only executed upon contract creation.
+    /// Instanciate the contract.
+    /// @param defaultNumber the initial value to store
+    /// @dev stores the number in the state variable `number`
     constructor(uint256 defaultNumber) {
-        // Accepts a string argument `defaultNumber` and sets the value into the contract's `number` storage variable).
         number = defaultNumber;
     }
 
-    // A public function that accepts a uint argument and updates the `number` storage variable.
+    /// Store `newNumber`.
+    /// @param newNumber the new value to store
+    /// @dev stores the number in the state variable `number`
     function storeNumber(uint256 newNumber) external {
         number = newNumber;
     }
 
-    // A public function that accepts a uint argument and updates the `number` storage variable.
+    /// Return the stored value.
+    /// @dev retrieves the value of the state variable `number`
+    /// @return the stored value
     function retrieveNumber() external view returns (uint256) {
         return number;
     }
